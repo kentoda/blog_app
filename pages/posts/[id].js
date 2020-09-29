316eac27ad6bf08d875d8452ed812ba4b93e6dd7
kentoda <kent19950811@gmail.com>
2020-09-29 22:29:37 +0900

    markdown rendering In of the middle work

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,20 @@
 import Layout  from '../../components/layout'
 import { getAllPostIds, getPostData} from '../../lib/posts'
+import Head from 'next/head'
 
 export default function Post({ postData }) {
   return (
   <Layout>
-    {postData.title}
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
     <br />
     {postData.id}
     <br />
-    {postData.data}
+    {postData.date}
+    <br />
+    {/* 非推奨？ */}
+    <div dangerouslySetInnerHTML={{__html: postData.contentHtml }} />
   </Layout>
   )
 }
@@ -24,7 +30,7 @@ export async function getStaticPaths() {
 
 // buildする時にページのコンテンツは何なのか,そのデータはどこにあるのかをgetStaticProps を使い取得する
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
